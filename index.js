32adb8e4ab6dc77332338187d45af3fd2f1d5cea
Yathaarth Batra <yathaarthbatra10@gmail.com>
2020-10-11 19:19:57 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("Hello world");
-console.log(:h)
+// In synchronous system the tasks are executed in sequence i.e.after
+// one task other task will be executed
+// In Async, if one task has started executing, in mean time other task
+// can get executed
+// Thus Asyn is better than Sync
+
+// In synchronous system we always use call back function.
+// A call back function is called after the execution of the task
+// It also takes arguement error which is used to tell the error in the execution of task
